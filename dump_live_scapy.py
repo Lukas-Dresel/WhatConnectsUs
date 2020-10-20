903391f9b1d1356fd565df9292ed81333355bdcc
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2020-10-20 16:26:38 -0400

    fixed dump_live_scapy to contain all ports

--- dump_live_scapy.py
+++ dump_live_scapy.py
@@ -6,7 +6,7 @@ import hexdump
 from parsing.enums import SendOption
 from parsing.network_packets import Packet_Server, Packet_Client
 
-
+PORTS_OF_INTEREST = {22023 + i for i in range(0, 1000, 100)}
 def pkt_callback(pkt):
     ip = pkt[IP] if IP in pkt else None
     udp = pkt[UDP] if UDP in pkt else None
@@ -14,8 +14,9 @@ def pkt_callback(pkt):
         return
     
     direction = (udp.sport, udp.dport)
-    if ARGS.port not in direction:
+    if udp.sport not in PORTS_OF_INTEREST and udp.dport not in PORTS_OF_INTEREST:
         return
+
     data = bytes(udp.payload)
     if data[0] in {SendOption.Ping.intvalue, SendOption.Acknowledgement.intvalue}:
         return
@@ -23,19 +24,20 @@ def pkt_callback(pkt):
     print(repr(data))
     description = '{:05d}->{:05d} {:2d}: {}'.format(udp.sport, udp.dport, data[0], hexdump.dump(data[1:]))
     print(description)
-    Packet = Packet_Client if direction[1] == ARGS.port else Packet_Server
+    Packet = Packet_Client if direction[1] in PORTS_OF_INTEREST else Packet_Server
     parsed_pkt = Packet.parse(data)
     print(parsed_pkt)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default=22023)
-    parser.add_argument('--source', type=str, default='live')
+    parser.add_argument('source', type=str, nargs='?', default='live')
     ARGS = parser.parse_args()
 
     print("Listening ...")
     if ARGS.source == 'live':
-        sniff(prn=pkt_callback, filter=f"udp port {ARGS.port}", store=0)
+        filter = ' or '.join(f'udp port {port}' for port in sorted(PORTS_OF_INTEREST))
+        print(filter)
+        sniff(prn=pkt_callback, filter=filter, store=0)
     else:
         sniff(offline=ARGS.source, prn=pkt_callback)
